4592c7c19e6bb93ba1bc60e1d638ee14c6dc682f
adoool <adibjadidi74@gmail.com>
2026-05-21 00:32:07 +0330

    feat(profile): implement live iphone preview panel with dynamic tailwind inline styles

--- src/features/profile/component/PreviewPanel.tsx
+++ src/features/profile/component/PreviewPanel.tsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { useProfileStore } from "../store/useProfileStore";
 
 const PreviewPanel = () => {
-  return <div>PreviewPanel</div>;
+  const { profile } = useProfileStore();
+  const { name, bio, avatarUrl, theme } = profile;
+  return (
+    <div className="w-[320px] h-[640px] flex flex-col bg-zinc-950 border-8 border-zinc-800 relative rounded-[40px] shadow-2xl overflow-hidden">
+      <div className="absolute top-3 left-1/2 -translate-x-1/2 w-28 h-4 bg-zinc-800 rounded-full z-10" />
+      <div
+        className="flex-1 p-6 flex flex-col pt-8 items-center overflow-y-auto no-scrollbar h-full"
+        style={{ backgroundColor: theme.backgroundColor }}
+      >
+        <div className="w-20 h-20 border-zinc-700/50 border-2 rounded-full bg-zinc-800 overflow-hidden flex items-center justify-center shadow-md">
+          {avatarUrl ? (
+            <img
+              src={avatarUrl}
+              alt={name}
+              className="w-full h-full object-cover"
+              onError={(e) => {
+                // اگر لینک عکس خراب بود، یک ظاهر دیفالت نشان بده
+                (e.target as HTMLImageElement).src =
+                  "https://api.dicebear.com/7.x/bottts/svg?seed=Felix";
+              }}
+            />
+          ) : (
+            // اگر کاربر هنوز عکسی نگذاشته بود، حرف اول اسمش را نشان بده
+            <span className="text-xl font-bold text-zinc-400">
+              {name ? name.charAt(0).toUpperCase() : "?"}
+            </span>
+          )}
+        </div>
+        <div>
+          <h2
+            className={`mt-4 text-lg font-bold text-center `}
+            style={{ color: theme.buttonBackgroundColor }}
+          >
+            {name || "Your Name"}
+          </h2>
+        </div>
+        <div>
+          <h2
+            className={`mt-4 text-xs font-bold text-center max-w-[200px] break-words leading-relaxed `}
+            style={{ color: theme.buttonBackgroundColor }}
+          >
+            {bio || "Your bio will appear here..."}
+          </h2>
+        </div>
+
+        <div className="w-full border-b border-zinc-800/50 my-6" />
+
+        <div className="w-full flex-1 space-y-2">
+          <div className="text-[10px] text-zinc-600 text-center mt-8 italic">
+            No linkes added yet
+          </div>
+        </div>
+
+        <div>
+          <span className="text-[10px] text-zinc-500 tracking-widest font-semibold uppercase opacity-60">
+            ⚡ LinkSnap
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PreviewPanel;
